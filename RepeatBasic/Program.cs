0d81ff28e8b78966427c03132ca1ac08908e271d
User <User@WIN-R1OMRL0S822>
2023-09-22 22:22:42 +0500

    Add project files.

---
+++ RepeatBasic/Program.cs
@@ -0,0 +1,43 @@
+﻿#region 1 dan 1000 gacha
+//int i = 1;
+//int sum = 0;
+//while (i <= 1000)
+//{
+//    sum += i;
+//    i++;
+//}
+//Console.WriteLine(sum);
+#endregion
+
+#region Array ichidan minimum va maximum qiymatni kupaytmasini topish
+//int min, max;
+//int[] array = {12, 20, 3, 11, 8};
+//min = max = array[0];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (min > array[i])
+//    {
+//        min = array[i];
+//    }
+//    if (max < array[i])
+//    {
+//        max = array[i];
+//    }
+//}
+//Console.WriteLine($"{max * min}");
+#endregion
+
+#region factorial
+//int x;
+//Console.Write("x = ");
+//x = Convert.ToInt32(Console.ReadLine());
+//int i = 1;
+//int x_factorial = 1;
+//while (x >= i)
+//{
+//    x_factorial *= i;
+//    i ++;
+//}
+//Console.WriteLine(x_factorial);
+#endregion
+
